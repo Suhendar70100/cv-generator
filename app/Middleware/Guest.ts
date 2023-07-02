078942dab254d717676266d5c95d5c595d23bc1a
ansthsys <azmiythufail91@gmail.com>
2023-07-03 02:54:36 +0700

    fix: change authenticate to check only the request

--- app/Middleware/Guest.ts
+++ app/Middleware/Guest.ts
@@ -2,7 +2,7 @@ import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 
 export default class Guest {
   public async handle({ auth, response }: HttpContextContract, next: () => Promise<void>) {
-    await auth.use('web').authenticate()
+    await auth.use('web').check()
 
     if (auth.use('web').isLoggedIn) {
       return response.redirect('back')
