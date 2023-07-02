7b463cddb8a957abbc89b2e3a9c939b621ea4255
ansthsys <azmiythufail91@gmail.com>
2023-07-03 01:30:40 +0700

    feat: create logic for handle user login

--- app/Controllers/Http/AuthController.ts
+++ app/Controllers/Http/AuthController.ts
@@ -1,4 +1,5 @@
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import User from 'App/Models/User'
 
 export default class AuthController {
   public async login({ view }: HttpContextContract) {
@@ -9,7 +10,7 @@ export default class AuthController {
     return ally.use('github').redirect()
   }
 
-  public async ghCallback({ ally, response, session }: HttpContextContract) {
+  public async ghCallback({ ally, auth, response, session }: HttpContextContract) {
     const github = ally.use('github')
 
     if (github.accessDenied() || github.stateMisMatch()) {
@@ -22,8 +23,22 @@ export default class AuthController {
       return response.redirect().toRoute('login')
     }
 
-    const user = await github.user()
+    const githubUser = await github.user()
 
-    return user
+    const user = await User.firstOrCreate(
+      {
+        email: githubUser.email,
+      },
+      {
+        name: githubUser.name,
+        avatarUrl: githubUser.avatarUrl,
+        isVerified: githubUser.emailVerificationState === 'verified',
+        provider: 'github',
+        accessToken: githubUser.token.token,
+      }
+    )
+
+    await auth.use('web').login(user)
+    response.redirect('/')
   }
 }
