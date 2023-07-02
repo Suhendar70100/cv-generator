e15e9e059f96d44c10776b6b9d080d16ae014906
ansthsys <azmiythufail91@gmail.com>
2023-07-03 01:29:38 +0700

    refactor: change field user in table database

--- database/migrations/1688020106967_users.ts
+++ database/migrations/1688020106967_users.ts
@@ -6,9 +6,12 @@ export default class extends BaseSchema {
   public async up() {
     this.schema.createTable(this.tableName, (table) => {
       table.increments('id').primary()
+      table.string('name', 100).notNullable()
       table.string('email', 255).notNullable().unique()
-      table.string('password', 180).notNullable()
-      table.string('remember_me_token').nullable()
+      table.text('avatar_url').nullable()
+      table.boolean('is_verified').notNullable()
+      table.string('provider', 100).notNullable()
+      table.string('access_token', 128).notNullable().unique()
 
       /**
        * Uses timestampz for PostgreSQL and DATETIME2 for MSSQL
