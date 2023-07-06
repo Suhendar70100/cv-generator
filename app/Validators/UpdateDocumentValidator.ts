4839ccacbb46ef21d21712a822c5785099986655
ansthsys <azmiythufail91@gmail.com>
2023-07-06 15:06:12 +0700

    feat: create validator class for update document

---
+++ app/Validators/UpdateDocumentValidator.ts
@@ -0,0 +1,55 @@
+import { schema, rules, CustomMessages } from '@ioc:Adonis/Core/Validator'
+import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+
+export default class UpdateDocumentValidator {
+  constructor(protected ctx: HttpContextContract) {}
+
+  /*
+   * Define schema to validate the "shape", "type", "formatting" and "integrity" of data.
+   *
+   * For example:
+   * 1. The username must be of data type string. But then also, it should
+   *    not contain special characters or numbers.
+   *    ```
+   *     schema.string({}, [ rules.alpha() ])
+   *    ```
+   *
+   * 2. The email must be of data type string, formatted as a valid
+   *    email. But also, not used by any other user.
+   *    ```
+   *     schema.string({}, [
+   *       rules.email(),
+   *       rules.unique({ table: 'users', column: 'email' }),
+   *     ])
+   *    ```
+   */
+  public schema = schema.create({
+    editName: schema.string([
+      rules.alphaNum({
+        allow: ['space', 'underscore', 'dash'],
+      }),
+      rules.minLength(3),
+      rules.maxLength(50),
+      rules.trim(),
+      rules.escape(),
+    ]),
+  })
+
+  /**
+   * Custom messages for validation failures. You can make use of dot notation `(.)`
+   * for targeting nested fields and array expressions `(*)` for targeting all
+   * children of an array. For example:
+   *
+   * {
+   *   'profile.username.required': 'Username is required',
+   *   'scores.*.number': 'Define scores as valid numbers'
+   * }
+   *
+   */
+  public messages: CustomMessages = {
+    'editName.required': 'Name is required',
+    'editName.alphaNum': 'Name must be a alphanumeric',
+    'editName.minLength': 'Name minimum length is {{ options.minLength }} characters',
+    'editName.maxLength': 'Name maximum length is {{ options.maxLength }} characters',
+  }
+}
