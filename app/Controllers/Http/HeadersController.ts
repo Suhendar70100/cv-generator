9922ec6a1f17f881920522f89e106d4ea2048f98
ansthsys <azmiythufail91@gmail.com>
2023-07-08 02:07:04 +0700

    feat: create method update in header controller

--- app/Controllers/Http/HeadersController.ts
+++ app/Controllers/Http/HeadersController.ts
@@ -1,9 +1,18 @@
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import Header from 'App/Models/Header'
+import ShortUniqueId from 'short-unique-id'
 
 export default class HeadersController {
   public async index({ params, view }: HttpContextContract) {
     const header = await Header.findBy('doc_id', params.docId)
     return view.render('header.index', { header, params })
+  }
+
+  public async update({ request, response }: HttpContextContract) {
+    const uid = new ShortUniqueId({ length: 10 })()
+    const body = request.body()
+    body.id = body.id || uid
+    const header = await Header.updateOrCreate({ docId: body.docId }, { ...body })
+    header ? response.redirect().toRoute('header', [body.docId]) : response.redirect().back()
   }
 }
