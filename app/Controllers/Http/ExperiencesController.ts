f1f222571147b3200470f5f85dd5980bf8160f99
ansthsys <azmiythufail91@gmail.com>
2023-07-10 17:00:48 +0700

    feat: create method show in experience controller

--- app/Controllers/Http/ExperiencesController.ts
+++ app/Controllers/Http/ExperiencesController.ts
@@ -1,9 +1,20 @@
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import Experience from 'App/Models/Experience'
 
 export default class ExperiencesController {
   public async index({ request, view }: HttpContextContract) {
     const document = request.document
     const experiences = await document.related('experiences').query().select('*')
     return view.render('experiences/index', { document, experiences })
+  }
+
+  public async show({ params, request, view }: HttpContextContract) {
+    const document = request.document
+    const experience = await Experience.query()
+      .where('id', params.expId)
+      .where('docId', document.id)
+      .firstOrFail()
+
+    return view.render('experiences/edit', { document, experience })
   }
 }
