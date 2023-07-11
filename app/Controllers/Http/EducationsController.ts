26444aa16337b9b04e8b16c9da51d17f5ae87e7b
ansthsys <azmiythufail91@gmail.com>
2023-07-11 16:01:35 +0700

    feat: create method index in education controller

---
+++ app/Controllers/Http/EducationsController.ts
@@ -0,0 +1,9 @@
+import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+
+export default class EducationsController {
+  public async index({ request, view }: HttpContextContract) {
+    const document = request.document
+    const educations = await document.related('educations').query().select('*')
+    return view.render('educations/index', { document, educations })
+  }
+}
